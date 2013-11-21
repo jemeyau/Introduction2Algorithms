1bae6204ebb04948654af535a5ea5f4fe5e62d52
youjh90 <youjh1990@gmail.com>
2013-11-21 20:56:33 +0800

    insertSorting complete

--- insertSorting.cpp
+++ insertSorting.cpp
@@ -1,3 +1,24 @@
 #include <iostream>
 
 using namespace std;
+
+int main()
+{
+	int a[] = {31,41, 59, 26, 41, 58};
+	for(int j=1; j<6; j++)
+	{
+		int key = a[j];
+		int i = j - 1;
+		while(i>=0 && a[i]>key)
+		{
+			a[i+1] = a[i];
+			i--;
+		}
+		a[i+1] = key;
+	}
+	for(int j=0; j<6; j++)
+	{
+		cout << a[j] << " ";
+	}
+	cout << endl;
+}
